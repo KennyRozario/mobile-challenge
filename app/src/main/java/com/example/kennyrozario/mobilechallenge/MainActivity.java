9e131fa97ca7a487dc1651037364a4bf092316af
Kenny Rozario <kmrozario98@gmail.com>
2017-01-21 21:48:55 -0500

    First commit

--- app/src/main/java/com/example/kennyrozario/mobilechallenge/MainActivity.java
+++ app/src/main/java/com/example/kennyrozario/mobilechallenge/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.kennyrozario.mobilechallenge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView gridView = (GridView) findViewById(R.id.grid_view);
+        gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //TODO: go to new screen with expanded image and enable to comeback to same spot
+            }
+        });
     }
 }
